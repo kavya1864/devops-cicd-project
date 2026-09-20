be54df4866b80f19deaf76e887e87c8726d4bf2c
kavya1864 <kavyasree1864@gmail.com>
2026-09-20 19:32:41 +0530

    updated polling file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello Kavya! GitHub polling is working!"
+    return "Hello Kavya! How are you doing !GitHub polling is working!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
